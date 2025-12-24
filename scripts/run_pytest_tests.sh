88f6bf6eb697584642c112ac4b23132171b15d36
Alisteras <artem.andronov86@gmail.com>
2025-12-24 09:28:42 +0700

    Update run_pytest_tests.sh

--- scripts/run_pytest_tests.sh
+++ scripts/run_pytest_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+cd ./reports
+touch pytest_report.html
 cd ./tests/pytest
 pytest -v
-python3 -m pytest -v test_redfish.py > test_redfish.txt 2>&1
+python3 -m pytest -v test_redfish.py  --html=../../reports/locust_report.html
 cd ../../
+#!/bin/bash
