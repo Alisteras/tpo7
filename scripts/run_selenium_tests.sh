807d1734cd502e0648c01997776c74f1a06aa0f2
Alisteras <artem.andronov86@gmail.com>
2025-12-24 11:35:33 +0700

    Update run_selenium_tests.sh

--- scripts/run_selenium_tests.sh
+++ scripts/run_selenium_tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-cd ./reports
-touch selenium_report.html
-cd ../
+
 cd ./tests/selenium
-rm -rf selenium_env
-python3 -m venv selenium_env
-source selenium_env/bin/activate
-pip install -r requirements.txt
-python openbmc_auth_tests.py --html=../../reports/selenium_report.html
+python openbmc_auth_tests.py 
 cd ../../
 #!/bin/bash
