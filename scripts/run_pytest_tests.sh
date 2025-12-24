2083d6cd74d81eb1eee3b463128ce0c28a7915e2
Alisteras <artem.andronov86@gmail.com>
2025-12-24 12:18:23 +0700

    Update run_pytest_tests.sh

--- scripts/run_pytest_tests.sh
+++ scripts/run_pytest_tests.sh
@@ -9,6 +9,6 @@ python3 -m venv pytest_env
 source pytest_env/bin/activate
 pip install -r requirements.txt
 pytest -v 
-python3 -m pytest -v test_redfish.py 
+python3 -m pytest -v test_redfish.py   --html=../../reports/selenium_report.html
 cd ../../
 #!/bin/bash
