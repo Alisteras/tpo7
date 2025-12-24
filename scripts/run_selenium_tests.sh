9a6cd24fd16947e8cf815be3c149a064cd5c2aed
Alisteras <artem.andronov86@gmail.com>
2025-12-24 12:19:10 +0700

    Update run_selenium_tests.sh

--- scripts/run_selenium_tests.sh
+++ scripts/run_selenium_tests.sh
@@ -8,6 +8,6 @@ rm -rf selenium_env
 python3 -m venv selenium_env
 source selenium_env/bin/activate
 pip install -r requirements.txt
-python openbmc_auth_tests.py  --html=../../reports/locust_report.html
+python openbmc_auth_tests.py  --html=../../reports/selenium_report.html
 cd ../../
 #!/bin/bash
