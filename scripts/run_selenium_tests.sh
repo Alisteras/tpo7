912639509890b3a5c08612b5deefe32533e50fc3
Alisteras <artem.andronov86@gmail.com>
2025-12-24 09:38:53 +0700

    Update run_selenium_tests.sh

--- scripts/run_selenium_tests.sh
+++ scripts/run_selenium_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+cd ./reports
+touch pytest_report.html
 cd ./tests/selenium
-python openbmc_auth_tests.py
+python openbmc_auth_tests.py --html=../../reports/pytest_report.html
 cd ../../
+#!/bin/bash
