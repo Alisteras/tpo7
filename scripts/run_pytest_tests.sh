b9089ab13f6cf45e538f5194f825a7b325dbaac3
Alisteras <artem.andronov86@gmail.com>
2025-12-24 10:52:35 +0700

    Update run_pytest_tests.sh

--- scripts/run_pytest_tests.sh
+++ scripts/run_pytest_tests.sh
@@ -3,6 +3,10 @@ cd ./reports
 touch pytest_report.html
 cd ../
 cd ./tests/pytest
+rm -rf pytest_env
+python3 -m venv pytest_env
+source pytest_env/bin/activate
+pip install -r requirements.txt
 pytest -v 
 python3 -m pytest -v test_redfish.py  --html=../../reports/pytest_report.html
 cd ../../
